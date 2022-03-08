303c469c32c6b45cf5ca0875a46ee6f9fab9b9ed
Chris Tate <chris_tate_94@hotmail.com>
2022-03-08 19:33:36 +0000

    added some checks on the emplpyee table

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,9 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
-@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 40)
 @store1.employees.create(first_name: "Mìcheil", last_name: "Kadmos", hourly_rate: 60)
-@store1.employees.create(first_name: "Christen", last_name: "Sima", hourly_rate: 80)
+@store1.employees.create(first_name: "Christen", last_name: "Sima", hourly_rate: 150)
 @store2.employees.create(first_name: "Bao", last_name: "Eline", hourly_rate: 60)
-@store2.employees.create(first_name: "Chris", last_name: "Tate", hourly_rate: 80)
+@store2.employees.create(first_name: "Chris", last_name: "Tate", hourly_rate: 200)
 @store2.employees.create(first_name: "Jessica", last_name: "Semen", hourly_rate: 60)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,22 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee < ActiveRecord::Base
+
+    validates :first_name, presence: 
+    true
+    validates :last_name, presence: 
+    true
+    validates :hourly_rate, numericality: { greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+    validates :store, absence: true
+
+end
+
+# class Store < ActiveRecord::Base
+
+#     validates :name, presence: 
+#     true
+#     valid
+
+# end
